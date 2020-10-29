053d49a8605744a30c64c57c88165e6d7098466a
mrosenbaum1 <64651384+mrosenbaum1@users.noreply.github.com>
2020-10-29 11:33:07 -0700

    routes.js updated

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,16 @@
-import {Routes, } from "react-router-dom";
+import React from 'react';
+import {
+  Switch,
+  Routes,
+  Redirect
+} from 'react-router-dom';
 
+function Routes({dogs}) {
+    return (
+        <Switch>
+            <Route exact path="/dogs"></Route>
+        </Switch>
+    )
+}
 
 export default Routes;
